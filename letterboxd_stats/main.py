faf089656ccbb288adcdfa31a82cb6a360024972
mBaratta96 <marcobaratta96@gmail.com>
2022-11-19 19:43:17 +0100

    argparse

--- letterboxd_stats/main.py
+++ letterboxd_stats/main.py
@@ -1,11 +1,15 @@
 from letterboxd_stats.tmdb import get_person
 from letterboxd_stats.data import read_watched_films
 from letterboxd_stats.cli import render_table
+import argparse
 import os
 
+parser = argparse.ArgumentParser(prog='Letterboxd Stats', description="CLI tool to display Letterboxd statistics")
+parser.add_argument('-s', '--search')
 
 if __name__ == '__main__':
-    df, name = get_person("akira kurosawa")
+    args = parser.parse_args()
+    df, name = get_person(args.search)
     path = os.path.join(os.environ['ROOT_FOLDER'], 'static', 'watched.csv')
     df = read_watched_films(df, path)
     render_table(df, name)
