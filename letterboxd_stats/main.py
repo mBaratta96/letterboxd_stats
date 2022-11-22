8461ca4c3e02f37fe019c10eba70d10ce9cb5075
mBaratta96 <marcobaratta96@gmail.com>
2022-11-22 21:05:21 +0100

    download file

--- letterboxd_stats/main.py
+++ letterboxd_stats/main.py
@@ -1,7 +1,7 @@
 from letterboxd_stats.tmdb import get_person
 from letterboxd_stats.data import read_watched_films
 from letterboxd_stats.cli import render_table
-from letterboxd_stats.web_scraper import login
+from letterboxd_stats.web_scraper import login, download_stats
 import argparse
 import os
 
@@ -15,3 +15,4 @@ if __name__ == '__main__':
     df = read_watched_films(df, path)
     render_table(df, name)
     login()
+    download_stats()
--- letterboxd_stats/web_scraper.py
+++ letterboxd_stats/web_scraper.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
+import time
 
-web = webdriver.Firefox()
+profile = webdriver.FirefoxProfile()
+profile.set_preference("browser.download.folderList", 2)
+profile.set_preference("browser.download.manager.showWhenStarting", False)
+profile.set_preference("browser.download.dir", os.path.join(os.environ['ROOT_FOLDER'], 'static'))
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
+
+web = webdriver.Firefox(firefox_profile=profile)
 web.implicitly_wait(10)
 web.get("https://letterboxd.com/")
 web.find_element(By.CLASS_NAME, "fc-cta-do-not-consent").click()
@@ -15,3 +25,12 @@ def login():
     password_input = web.find_element(By.ID, 'password')
     password_input.send_keys(os.environ['LETTERBOXD_PASSWORD'])
     web.find_element(By.CLASS_NAME, "button-container").click()
+
+def download_stats():
+    nav_account = web.find_element(By.CLASS_NAME, "nav-account")
+    ActionChains(web).move_to_element(nav_account).perform()
+    web.find_element(By.LINK_TEXT, 'Settings').click()
+    web.find_element(By.XPATH, "//a[@data-id='data']").click()
+    web.find_element(By.CLASS_NAME, 'export-data-link').click()
+    WebDriverWait(web, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'export-data-button'))).click()
+    time.sleep(10)
