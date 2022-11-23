f9cf7e47fba0226082a55f4f7c0e180d881f1326
mBaratta96 <marcobaratta96@gmail.com>
2022-11-23 12:29:51 +0100

    extract zip

--- letterboxd_stats/main.py
+++ letterboxd_stats/main.py
@@ -1,18 +1,23 @@
 from letterboxd_stats.tmdb import get_person
 from letterboxd_stats.data import read_watched_films
 from letterboxd_stats.cli import render_table
-from letterboxd_stats.web_scraper import login, download_stats
+from letterboxd_stats import web_scraper as ws
 import argparse
 import os
 
-parser = argparse.ArgumentParser(prog='Letterboxd Stats', description="CLI tool to display Letterboxd statistics")
-parser.add_argument('-s', '--search')
+parser = argparse.ArgumentParser(
+    prog='Letterboxd Stats', description="CLI tool to display Letterboxd statistics")
+parser.add_argument('-s', '--search', help="Search for a director")
+parser.add_argument('-d', '--download',
+                    help="Download letterboxd data from your account", action="store_true")
 
 if __name__ == '__main__':
     args = parser.parse_args()
+    if args.download:
+        ws.login()
+        ws.download_stats()
+        ws.extract_data()
     df, name = get_person(args.search)
     path = os.path.join(os.environ['ROOT_FOLDER'], 'static', 'watched.csv')
     df = read_watched_films(df, path)
     render_table(df, name)
-    login()
-    download_stats()
