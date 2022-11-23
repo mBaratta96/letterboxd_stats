130dc859a8d9fcc05a4734be4c744dfa75cfe3b6
mBaratta96 <marcobaratta96@gmail.com>
2022-11-23 12:29:51 +0100

    extract zip

--- letterboxd_stats/data.py
+++ letterboxd_stats/data.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 
+
 def read_watched_films(df: pd.DataFrame, path: str) -> pd.DataFrame:
     df_profile = pd.read_csv(path)
-    df.insert(0, 'watched', np.where(df['title'].isin(df_profile['Name']), '[X]', '[ ]')) 
+    df.insert(0, 'watched', np.where(
+        df['title'].isin(df_profile['Name']), '[X]', '[ ]'))
     return df
--- letterboxd_stats/web_scraper.py
+++ letterboxd_stats/web_scraper.py
@@ -1,21 +1,30 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 import time
+from zipfile import ZipFile
+
 
 profile = webdriver.FirefoxProfile()
 profile.set_preference("browser.download.folderList", 2)
 profile.set_preference("browser.download.manager.showWhenStarting", False)
-profile.set_preference("browser.download.dir", os.path.join(os.environ['ROOT_FOLDER'], 'static'))
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
+profile.set_preference("browser.download.dir", os.path.join(
+    os.environ['ROOT_FOLDER'], 'static'))
+profile.set_preference(
+    "browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
 
-web = webdriver.Firefox(firefox_profile=profile)
+options = Options()
+options.add_argument("--headless")
+
+web = webdriver.Firefox(firefox_profile=profile, options=options)
 web.implicitly_wait(10)
 web.get("https://letterboxd.com/")
 web.find_element(By.CLASS_NAME, "fc-cta-do-not-consent").click()
+
 
 def login():
     sign_in_button = web.find_element(By.CLASS_NAME, "sign-in-menu")
@@ -25,6 +34,8 @@ def login():
     password_input = web.find_element(By.ID, 'password')
     password_input.send_keys(os.environ['LETTERBOXD_PASSWORD'])
     web.find_element(By.CLASS_NAME, "button-container").click()
+    print("Login successful")
+
 
 def download_stats():
     nav_account = web.find_element(By.CLASS_NAME, "nav-account")
@@ -32,5 +43,23 @@ def download_stats():
     web.find_element(By.LINK_TEXT, 'Settings').click()
     web.find_element(By.XPATH, "//a[@data-id='data']").click()
     web.find_element(By.CLASS_NAME, 'export-data-link').click()
-    WebDriverWait(web, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'export-data-button'))).click()
-    time.sleep(10)
+    WebDriverWait(web, 10).until(EC.element_to_be_clickable(
+        (By.CLASS_NAME, 'export-data-button'))).click()
+    time.sleep(5)
+    print("Data download successful")
+
+
+def extract_data():
+    archive = None
+    path = os.path.expanduser(os.path.join(
+        os.environ['ROOT_FOLDER'], 'static'))
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_file() and entry.name.endswith(".zip") and entry.name.startswith('letterboxd'):
+                archive = entry.path
+                break
+    if not archive:
+        raise FileNotFoundError("No Letterboxd .zip file found.")
+    with ZipFile(archive, 'r') as zip:
+        zip.extractall(path)
+    os.remove(archive)
