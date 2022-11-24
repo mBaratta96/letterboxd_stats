d745233bbb2f158a92a0b7f4a1fa6014bb9fa87a
mBaratta96 <marcobaratta96@gmail.com>
2022-11-24 22:47:00 +0100

    select department

--- letterboxd_stats/tmdb.py
+++ letterboxd_stats/tmdb.py
@@ -1,23 +1,30 @@
 from dotenv import load_dotenv
-from tmdbv3api import Person 
+from tmdbv3api import Person
 import pandas as pd
+from letterboxd_stats.cli import select_department
 
 person = Person()
 
-def get_person(name: str) -> tuple[pd.DataFrame, str]:
-    result = person.search(name)[0] 
-    p = person.details(result["id"])
-    known_department = p["known_for_department"]
-    
-    movie_credits = person.movie_credits(result["id"])
+
+def get_person(name: str):
+    search_result = person.search(name)[0]
+    return search_result
+
+
+def create_person_dataframe(search_result):
+    p = person.details(search_result["id"])
+    known_for_department = p['known_for_department']
+    movie_credits = person.movie_credits(search_result["id"])
     list_of_films = [{
         "title": movie.get('title'),
         "release_date": movie.get('release_date'),
         "department": movie.get('department'),
     } for movie in movie_credits["crew"]]
-   
     df = pd.DataFrame(list_of_films)
+    department = select_department(
+        df['department'].unique(), known_for_department
+    )
+    df = df[df['department'] == department]
     df['release_date'] = pd.to_datetime(df['release_date'])
     df.sort_values(by="release_date", inplace=True)
-    df = df[df["department"] == known_department]
-    return df, result["name"]
+    return df
