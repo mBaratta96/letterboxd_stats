b959208546bf753e2fad4c981414f77d47370fe1
mBaratta96 <marcobaratta96@gmail.com>
2022-11-25 17:39:35 +0100

    black formatting

--- letterboxd_stats/data.py
+++ letterboxd_stats/data.py
@@ -4,7 +4,5 @@ import numpy as np
 
 def read_watched_films(df: pd.DataFrame, path: str) -> pd.DataFrame:
     df_profile = pd.read_csv(path)
-    df.insert(0, 'watched', np.where(
-        df['title'].isin(df_profile['Name']), '[X]', '[ ]')
-    )
+    df.insert(0, "watched", np.where(df["title"].isin(df_profile["Name"]), "[X]", "[ ]"))
     return df
--- letterboxd_stats/tmdb.py
+++ letterboxd_stats/tmdb.py
@@ -4,7 +4,7 @@ from letterboxd_stats.cli import select_department
 from config import config
 
 tmdb = TMDb()
-tmdb.api_key = config['TMDB']['api_key']
+tmdb.api_key = config["TMDB"]["api_key"]
 person = Person()
 
 
@@ -15,18 +15,19 @@ def get_person(name: str):
 
 def create_person_dataframe(search_result):
     p = person.details(search_result["id"])
-    known_for_department = p['known_for_department']
+    known_for_department = p["known_for_department"]
     movie_credits = person.movie_credits(search_result["id"])
-    list_of_films = [{
-        "title": movie.get('title'),
-        "release_date": movie.get('release_date'),
-        "department": movie.get('department'),
-    } for movie in movie_credits["crew"]]
+    list_of_films = [
+        {
+            "title": movie.get("title"),
+            "release_date": movie.get("release_date"),
+            "department": movie.get("department"),
+        }
+        for movie in movie_credits["crew"]
+    ]
     df = pd.DataFrame(list_of_films)
-    department = select_department(
-        df['department'].unique(), p['name'], known_for_department
-    )
-    df = df[df['department'] == department]
-    df['release_date'] = pd.to_datetime(df['release_date'])
+    department = select_department(df["department"].unique(), p["name"], known_for_department)
+    df = df[df["department"] == department]
+    df["release_date"] = pd.to_datetime(df["release_date"])
     df.sort_values(by="release_date", inplace=True)
     return df
