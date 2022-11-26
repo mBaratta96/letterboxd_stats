4d8ac82e46cb45d5f44563890802747e166cbf81
mBaratta96 <marcobaratta96@gmail.com>
2022-11-26 10:14:32 +0100

    wishlist

--- letterboxd_stats/data.py
+++ letterboxd_stats/data.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import numpy as np
+from letterboxd_stats.cli import render_table
 
 
 def read_watched_films(df: pd.DataFrame, path: str) -> pd.DataFrame:
     df_profile = pd.read_csv(path)
     df.insert(0, "watched", np.where(df["title"].isin(df_profile["Name"]), "[X]", "[ ]"))
     return df
+
+
+def show_wishlist(path: str, shuffle: bool, limit=None):
+    df = pd.read_csv(path)
+    if shuffle:
+        df = df.sample(frac=1)
+    if limit:
+        df = df.iloc[:limit, :]
+    render_table(df, "Wishlist")
