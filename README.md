e6d61ebca0cf800f7bacf22d72a467d02acda9d7
mBaratta96 <marcobaratta96@gmail.com>
2023-02-28 12:54:57 +0100

    readme test

---
+++ README.md
@@ -0,0 +1,3 @@
+# Letterboxd Stats
+
+Get information about your Letterboxd activity. Search for actors/directors, analyze your diary/watchlist/rating, check which film you have seen for a particular person. All in your terminal.
--- letterboxd_stats/web_scraper.py
+++ letterboxd_stats/web_scraper.py
@@ -8,6 +8,7 @@ URL = "https://letterboxd.com"
 LOGIN_PAGE = URL + "/user/login.do"
 DATA_PAGE = URL + "/data/export"
 
+
 class Downloader:
     def __init__(self):
         self.session = requests.Session()
@@ -15,9 +16,9 @@ class Downloader:
 
     def login(self):
         request_payload = {
-            'username': config['Letterboxd']['username'],
-            'password': config['Letterboxd']['password'],
-            '__csrf': self.session.cookies.get('com.xk72.webparts.csrf')
+            "username": config["Letterboxd"]["username"],
+            "password": config["Letterboxd"]["password"],
+            "__csrf": self.session.cookies.get("com.xk72.webparts.csrf"),
         }
         res = self.session.post(LOGIN_PAGE, data=request_payload)
         if res.json()["result"] != "success":
@@ -25,11 +26,13 @@ class Downloader:
 
     def download_stats(self):
         res = self.session.get(DATA_PAGE)
-        if res.status_code != 200 or "application/zip" not in res.headers['Content-Type']:            
+        if res.status_code != 200 or "application/zip" not in res.headers["Content-Type"]:
             raise ConnectionError(f"Impossible to download data. Response headers:\n{res.headers}")
         print("Data download successful.")
         filename = res.headers["content-disposition"].split()[-1].split("=")[-1]
         path = os.path.expanduser(os.path.join(config["root_folder"], "static"))
+        if not os.path.exists(path):
+            os.makedirs(path)
         archive = os.path.join(path, filename)
         with open(archive, "wb") as f:
             f.write(res.content)
@@ -40,15 +43,15 @@ class Downloader:
 
 def get_tmdb_id(link: str, is_diary: bool):
     res = requests.get(link)
-    movie_page = html.fromstring(res.text)    
+    movie_page = html.fromstring(res.text)
     if is_diary:
         title_link = movie_page.xpath("//span[@class='film-title-wrapper']/a")
         if len(title_link) > 0:
             movie_link = title_link[0]
-            movie_url = URL + movie_link.get('href')
+            movie_url = URL + movie_link.get("href")
             movie_page = html.fromstring(requests.get(movie_url).text, "lxml")
     tmdb_link = movie_page.xpath("//a[@data-track-action='TMDb']")
     if len(tmdb_link) > 0:
-        id = tmdb_link[0].get('href').split("/")[-2]
+        id = tmdb_link[0].get("href").split("/")[-2]
         return int(id)
     return None
